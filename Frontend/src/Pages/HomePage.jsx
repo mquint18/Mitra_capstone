0976ce9f9238dd9e4522c47f3cb0020b91ea0d11
Michael <michaelquint@gmail.com>
2026-07-30 15:55:05 -0400

    updated image link

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -1,46 +1,7 @@
-// // HomePage.jsx
-
-// import "./HomePage.css";
-
-// function HomePage() {
-//   return (
-//     <div className="home-wrap">
-//       <h1>Welcome to Mitra - Your Neighborhood, Connected</h1>
-//       <h2>How it Works</h2>
-//       <p>
-//         Mitra is a Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-//         Quibusdam, doloribus. Iure, illo. Vel possimus aut, quidem atque aperiam
-//         quae sit facere dolorem maiores necessitatibus ducimus. Saepe explicabo
-//         doloremque porro quibusdam. Lorem, ipsum dolor sit amet consectetur
-//         adipisicing elit. Quidem iusto quod perspiciatis odit natus maxime cum
-//         saepe, doloremque, ullam, dolores sint dolorum minima ab doloribus
-//         asperiores ipsum reiciendis modi! Consequatur.
-//       </p>
-
-//       <img
-//         className="welcome-image"
-//         src="src/assets/images/town.webp"
-//         alt="small town"
-//       />
-
-//       <p>
-//         Mitra is a Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-//         Quibusdam, doloribus. Iure, illo. Vel possimus aut, quidem atque aperiam
-//         quae sit facere dolorem maiores necessitatibus ducimus. Saepe explicabo
-//         doloremque porro quibusdam. Lorem, ipsum dolor sit amet consectetur
-//         adipisicing elit. Quidem iusto quod perspiciatis odit natus maxime cum
-//         saepe, doloremque, ullam, dolores sint dolorum minima ab doloribus
-//         asperiores ipsum reiciendis modi! Consequatur.
-//       </p>
-//     </div>
-//   );
-// }
-
-// export default HomePage;
-
 // HomePage.jsx
 import "./HomePage.css";
 import { useNavigate } from "react-router-dom";
+import townImg from "../assets/images/town.webp";
 
 function HomePage() {
   const navigate = useNavigate();
@@ -76,10 +37,9 @@ function HomePage() {
             </button>
           </div>
         </div>
-
         <img
-          className="home-hero-img"
-          src="src/assets/images/town.webp"
+          className="welcome-image"
+          src={townImg}
           alt="A friendly neighbourhood street"
         />
       </div>
